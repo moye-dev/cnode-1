6b51ea8f29d9063f0a814c191642002ff7fb86fc
moye <2801845775@qq.com>
2021-03-31 19:18:10 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,37 @@
+const myChart = echarts.init(document.getElementById('skills'));
+const option = {
+    tooltip: {},
+    radar: {
+        // shape: 'circle',
+        name: {
+            textStyle: {
+                color: '#fff',
+                backgroundColor: '#999',
+                borderRadius: 3,
+                padding: [3, 5]
+            }
+        },
+        indicator: [
+            { name: '静态页面', max: 100},
+            { name: '编程基础', max: 100},
+            { name: 'Vue', max: 100},
+            { name: '小程序', max: 100},
+            { name: '英语', max: 100},
+            { name: '沟通能力', max: 100}
+        ]
+    },
+    series: [{
+        name: '能力雷达',
+        type: 'radar',
+        // areaStyle: {normal: {}},
+        data: [
+            {
+                value: [90, 75, 75, 60, 70, 60],
+                name: '能力雷达'
+            },
+        ]
+    }]
+};
+
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
